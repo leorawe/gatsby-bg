7f4b80bf6aa3f3a3b078a10841912923799cad39
Leora Wenger <web@leoraw.com>
2019-06-30 08:38:02 -0400

    styled components, layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,57 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
+import styled from 'styled-components'
 
 import Header from "./header"
+import Menu from "./menu"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const BodyWrapper = styled.div `
+      margin: 0 auto;
+      backgroundColor: #ffffff;
+      maxWidth: 800;
+      padding: 0px 1.0875rem 1.45rem;
+      paddingTop: 0;
+      border: 1px solid #eee;
+`
+
+const Layout = ({ children, }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            description
+            headerLinks{
+              name
+              link
+            }
+          }
         }
       }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+    `}
+    render={data => (
+      <>
+        <Menu 
+        headerLinks={data.site.siteMetadata.headerLinks}
+        />
+        <Header siteTitle={data.site.siteMetadata.title}
+        siteDesc={data.site.siteMetadata.description}
+         />
+        <BodyWrapper>
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, copyright 
+            {` `}
+            Barbara Gelfand
+          </footer>
+        </BodyWrapper>
+      </>
+    )}
+  />
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
