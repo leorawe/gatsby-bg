57f59ce7e14bdcbb29736d71caca0940201e800e
Leora Wenger <web@leoraw.com>
2019-07-05 08:08:30 -0400

    specialty: added text

--- src/pages/specialty.js
+++ src/pages/specialty.js
@@ -10,7 +10,56 @@ const Education = () => (
     pagetitle = "Areas of Specialty"
 
     />
-    <p>Welcome to page a list of specialties</p>
+    <p>Barbara Gelfand LCSW works in these specialties:</p>
+    <ul>
+<li>	Anxiety</li>
+<li>	Depression</li>
+<li>	Eating Disorders
+</li>
+<li>	Relationship Issues
+</li>
+<li>	Marital Issues
+</li>
+<li>	Family Issues
+</li>
+<li>	Self-Esteem
+</li>
+<li>	Grief and Loss
+</li>
+<li>	Mindfulness
+</li>
+<li>	Mind/Body Somatic Techniques
+</li>
+<li>	Stress Reduction
+</li>
+<li>	Meditation
+</li>
+<li>	Spirituality
+</li>
+<li>	Chronic Disease Self-Management
+</li>
+<li>	Diabetes Counseling
+</li>
+<li>	Weight Loss
+</li>
+<li>	Physical Fitness
+</li>
+<li>	Pain Management
+</li>
+<li>	Sleep Disorders and Insomnia
+</li>
+<li>	Sexuality Concerns
+</li>
+<li>	Women’s Issues
+</li>
+<li>	Parenting Concerns
+</li>
+<li>	Adoption Concerns
+</li>
+<li>	Life Coaching
+</li>
+<li>	Life Transitions</li>
+</ul>
   </Layout>
 )
 
