93f4999e58000901a1455ecc06e704414c655523
Leora Wenger <web@leoraw.com>
2019-07-21 06:19:45 -0400

    specialty: ul two col

--- src/pages/specialty.js
+++ src/pages/specialty.js
@@ -1,7 +1,21 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from "styled-components"
+
 import Pagetitle from "../components/pagetitle"
+import media from '../components/mediaqueries'
+
+const DoubleUL = styled.ul`
+  columns: 2;
+  -webkit-columns: 2;
+  -moz-columns: 2;
+  ${media.tablet`
+  columns: 1;
+  -webkit-columns: 1;
+  -moz-columns: 1;
+  `} 
+`
 
 const Education = () => (
   <Layout>
@@ -14,55 +28,55 @@ const Education = () => (
 
     />
     <p>My practice focuses on clinical work with older adolescents and adults utilizing individual, group, family and couples modalities. My areas of expertise includes:</p>
-    <ul>
-<li>	Anxiety</li>
-<li>	Depression</li>
-<li>	Eating Disorders
-</li>
-<li>	Relationship Issues
-</li>
-<li>	Marital Issues
-</li>
-<li>	Family Issues
-</li>
-<li>	Self-Esteem
-</li>
-<li>	Grief and Loss
-</li>
-<li>	Mindfulness
-</li>
-<li>	Mind/Body Somatic Techniques
-</li>
-<li>	Stress Reduction
-</li>
-<li>	Meditation
-</li>
-<li>	Spirituality
-</li>
-<li>	Chronic Disease Self-Management
-</li>
-<li>	Diabetes Counseling
-</li>
-<li>	Weight Loss
-</li>
-<li>	Physical Fitness
-</li>
-<li>	Pain Management
-</li>
-<li>	Sleep Disorders and Insomnia
-</li>
-<li>	Sexuality Concerns
-</li>
-<li>	Women’s Issues
-</li>
-<li>	Parenting Concerns
-</li>
-<li>	Adoption Concerns
-</li>
-<li>	Life Coaching
-</li>
-<li>	Life Transitions</li>
-</ul>
+    <DoubleUL>
+      <li>	Anxiety</li>
+      <li>	Depression</li>
+      <li>	Eating Disorders
+      </li>
+      <li>	Relationship Issues
+      </li>
+      <li>	Marital Issues
+      </li>
+      <li>	Family Issues
+      </li>
+      <li>	Self-Esteem
+      </li>
+      <li>	Grief and Loss
+      </li>
+      <li>	Mindfulness
+      </li>
+      <li>	Mind/Body Somatic Techniques
+      </li>
+      <li>	Stress Reduction
+      </li>
+      <li>	Meditation
+      </li>
+      <li>	Spirituality
+      </li>
+      <li>	Chronic Disease Self-Management
+      </li>
+      <li>	Diabetes Counseling
+      </li>
+      <li>	Weight Loss
+      </li>
+      <li>	Physical Fitness
+      </li>
+      <li>	Pain Management
+      </li>
+      <li>	Sleep Disorders and Insomnia
+      </li>
+      <li>	Sexuality Concerns
+      </li>
+      <li>	Women’s Issues
+      </li>
+      <li>	Parenting Concerns
+      </li>
+      <li>	Adoption Concerns
+      </li>
+      <li>	Life Coaching
+      </li>
+      <li>	Life Transitions</li>
+    </DoubleUL>
   </Layout>
 )
 
