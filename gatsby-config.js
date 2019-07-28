d608fb779e085a7b0b490981f57ed40f5705b1f8
Leora Wenger <web@leoraw.com>
2019-07-28 07:32:36 -0400

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-144736683-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
